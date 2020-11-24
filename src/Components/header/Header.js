e8ccd5dfdaedecb1492c21141bd3f161f9c45be2
murilomendonca89 <murilomendonca@live.com>
2020-11-24 18:20:02 -0300

    header com links e rotas OK

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,22 +1,54 @@
 import React from "react";
-import {
-  Container,
-  Container2BotaoVoltar,
-  Container3BotaoPoke,
-} from "../header/styled";
+import { Container, ButtonLeft, ButtonRight, PokedexContainer, TitleContainer, Transparent } from "../header/styled";
+import { Switch, Route, useHistory } from 'react-router-dom'
 
 const Header = () => {
+
+  const history = useHistory()
+
+  const goToPokemonList= () => {
+      history.push("/");
+  }
+
+  const goToPokedex= () => {
+      history.push("/pokedex");
+  }
+
+  const goBack= () => {
+      history.goBack();
+  }
+
   return (
     <Container>
-      <div>
-        <Container3BotaoPoke>Ir para Pokedex</Container3BotaoPoke>
-      </div>
-      <h1>Lista de Pokemons</h1>
-      <div>
-        <Container2BotaoVoltar>Voltar</Container2BotaoVoltar>
-      </div>
+
+      <Switch>
+        <Route exact path="/pokedex">
+          <PokedexContainer>
+            <ButtonLeft onClick={goToPokemonList}>Ver todos os Pokemons</ButtonLeft>
+            <TitleContainer>Pokedex</TitleContainer>
+            <Transparent>essebotaoétransparente</Transparent>          
+          </PokedexContainer>
+        </Route>
+        
+        <Route exact path="/">
+          <PokedexContainer>
+            <ButtonLeft onClick={goToPokedex}>Ir para Pokedex</ButtonLeft>
+            <TitleContainer>Lista de Pokemons</TitleContainer>
+            <Transparent>essebotaoétransparente</Transparent>      
+          </PokedexContainer>
+        </Route>
+
+        <Route exact path="/pokedex/poke-detail">
+          <PokedexContainer>
+            <ButtonLeft onClick={goBack}>Voltar</ButtonLeft>
+            <TitleContainer>Nome do Pokemon</TitleContainer>
+            <ButtonRight onClick={goToPokemonList}>Ir para Pokedex</ButtonRight>            
+          </PokedexContainer>
+        </Route>
+      </Switch>
+
     </Container>
-  );
-};
+  )
+}
 
 export default Header;
